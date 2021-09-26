bfab8def10f38175ce0fea730cf0d888e5660236
Luigi Dalle aste <ldalle-a@e3r9p7.42roma.it>
2021-09-26 16:22:25 +0200

    norm monitoring.c

--- srcs/monitoring.c
+++ srcs/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: ldalle-a <ldalle-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 13:45:20 by ldalle-a          #+#    #+#             */
-/*   Updated: 2021/09/26 16:17:55 by ldalle-a         ###   ########.fr       */
+/*   Updated: 2021/09/26 16:22:08 by ldalle-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	meal_and_death_monitor(t_main *main)
 		time = ft_itoa(elapsed_time(main->start_time));
 		printf("%s\t", time);
 		printf("\033[32mEach philosopher has eaten %d times.\n\033[0m",
-		main->nb_meal);
+			main->nb_meal);
 		free(time);
 	}
 }
